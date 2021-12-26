13e7eee24cf4f6a868a61f1270e308abd9bd8f29
DeonDre White <93960184+deondre-white00@users.noreply.github.com>
2021-12-25 23:40:06 -0600

    Refresher Beginnings
    Early stages of refresher

---
+++ for_loop.py
@@ -0,0 +1,4 @@
+print('My name is')
+for i in range(5):
+    i += 1
+    print('Dre Five Times ' + str(i))
---
+++ if_elif_example.py
@@ -0,0 +1,10 @@
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('HI Alice')
+elif age < 12:
+    print('You are not Alice.')
+elif age > 2000:
+    print('Vampire in Brooklyn?')
+elif  age > 100:
+    print('I know you seen a lot')
